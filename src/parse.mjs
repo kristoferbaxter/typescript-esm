881729ed50fad9f75029ef0119481505f1e9f910
Kristofer Baxter <kristofer@kristoferbaxter.com>
2021-03-19 06:57:14 -0700

    Update to latest dependencies and remove unnecessary acorn extension with updated major revision

--- src/parse.mjs
+++ src/parse.mjs
@@ -1,6 +1,5 @@
 import { promises as fs } from 'fs';
-import acorn from 'acorn';
-import importMeta from 'acorn-import-meta';
+import * as acorn from 'acorn';
 import MagicString from 'magic-string';
 import { log } from './log.mjs';
 
@@ -19,7 +18,7 @@ const DEFAULT_ACORN_OPTIONS = {
 export async function parse(filePath) {
   try {
     const contents = await fs.readFile(filePath, 'utf8');
-    const parse = acorn.Parser.extend(importMeta).parse(contents, DEFAULT_ACORN_OPTIONS);
+    const parse = acorn.Parser.parse(contents, DEFAULT_ACORN_OPTIONS);
     const magic = new MagicString(contents);
     return [parse, magic];
   } catch (e) {
