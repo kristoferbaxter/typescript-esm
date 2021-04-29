1ed13e2dc31bed77a3691ca5280d834ec2acfd48
jaketrent <trent.jake@gmail.com>
2021-04-29 09:58:15 -0600

    update fixtures after assertion error discovery

--- test/outdir-convert/fixtures/direct.mjs
+++ test/outdir-convert/fixtures/direct.mjs
@@ -1,7 +1,7 @@
 import { imported } from './imported.mjs';
 export default function () {
-  const bar = 'bar';
-  console.log(bar);
-  imported();
+    const bar = 'bar';
+    console.log(bar);
+    imported();
 }
 //# sourceMappingURL=direct.mjs.map
