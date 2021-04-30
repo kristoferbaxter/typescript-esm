0b045a6a6509b68937067b20e18ab3057aa17a01
Kristofer Baxter <kristofer@kristoferbaxter.com>
2021-04-30 11:56:13 -0700

    Fix for imports with extensions besides js or mjs

--- test/outdir-convert/fixtures/direct.mjs
+++ test/outdir-convert/fixtures/direct.mjs
@@ -1,7 +1,9 @@
 import { imported } from './imported.mjs';
+import { manager } from './imported.manager.mjs';
 export default function () {
-  const bar = 'bar';
-  console.log(bar);
-  imported();
+    const bar = 'bar';
+    console.log(bar);
+    imported();
+    manager();
 }
 //# sourceMappingURL=direct.mjs.map
---
+++ test/outdir-convert/fixtures/imported.manager.mjs
@@ -0,0 +1,5 @@
+export function manager() {
+    const bar = 'bar';
+    console.log(bar);
+}
+//# sourceMappingURL=imported.manager.mjs.map
