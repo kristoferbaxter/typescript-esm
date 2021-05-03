8c49b63352e5e8be8b80698169e62f0a7ca838e4
Kristofer Baxter <kristofer@kristoferbaxter.com>
2021-05-03 08:18:17 -0700

    Formatting

--- test/outdir-convert/fixtures/direct.mjs
+++ test/outdir-convert/fixtures/direct.mjs
@@ -1,9 +1,9 @@
 import { imported } from './imported.mjs';
 import { manager } from './imported.manager.mjs';
 export default function () {
-    const bar = 'bar';
-    console.log(bar);
-    imported();
-    manager();
+  const bar = 'bar';
+  console.log(bar);
+  imported();
+  manager();
 }
 //# sourceMappingURL=direct.mjs.map
--- test/outdir-convert/fixtures/imported.manager.mjs
+++ test/outdir-convert/fixtures/imported.manager.mjs
@@ -1,5 +1,5 @@
 export function manager() {
-    const bar = 'bar';
-    console.log(bar);
+  const bar = 'bar';
+  console.log(bar);
 }
 //# sourceMappingURL=imported.manager.mjs.map
